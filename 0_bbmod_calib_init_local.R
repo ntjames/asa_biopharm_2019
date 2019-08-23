1eac40b339006f8d1dcfeef20332651b23112e74
Nathan T. James <nj1154@gmail.com>
2019-08-22 20:47:24 -0500

    update joint bb model Stan code

--- 0_bbmod_calib_init_local.R
+++ 0_bbmod_calib_init_local.R
@@ -2,7 +2,6 @@
 ## LOCAL version
 
 rm(list=ls())
- 
 library(rstan)
 
 # set working directory
@@ -61,23 +60,19 @@ p_s = Phi(beta_s);
 }
 "
 
-# joint models
-mod_bb_jnt_code <- "
+# joint models with different priors (if time, try diff copula, e.g. Frank)
+mod_bb_jnt_code1 <- "
+// model 1 - flat priors
+
 functions {
 #include /bb_normCop_funs.stan
 }
 data {
-int<lower=1> N;
-matrix[N, 2] x;
-int<lower=0, upper=1> y_e[N];
-int<lower=0, upper=1> y_s[N];
-int<lower=0> freq[N];
+#include /bb_short_dat.stan
 }
 parameters {
-// params for binary (efficacy) outcome
-vector[2] beta_e;
-
-//params for binary (safety) outcome
+// params for binary efficacy (beta_e) and safety (beta_s) outcomes
+vector[2] beta_e; 
 vector[2] beta_s;
 
 // copula dependence param
@@ -88,20 +83,13 @@ vector[N] pr_e;
 vector[N] pr_s;
 vector[N] theta;
 
-// priors
-// uniform priors (leave beta_e and beta_s unassigned)
-//rho ~ uniform(-1,1);
+// priors (unassigned)
+// improper (flat) priors for beta_e and beta_s 
+// uniform(-1,1) prior for rho
+// rho_ ~ uniform(-1,1);
 
-// regularizing priors
-// add hyperprior for variance of rho prior??
-beta_e ~ normal(0,10);
-beta_s ~ normal(0,10); 
-rho_ ~ normal(0,0.4); 
-
-// marginal probit for binary (efficacy) outcome
+// marginal probit models for binary efficacy and safety outcomes
 pr_e = Phi(x*beta_e);
-
-// marginal probit for binary (safety) outcome
 pr_s = Phi(x*beta_s);
 
 // copula dependence model
@@ -117,21 +105,125 @@ theta = x*rho_;
 
 }
 generated quantities {
-vector[2] p_e;
-vector[2] p_s;
-real p_e_diff;
-real p_s_diff;
+#include /bb_normCop_genquants.stan
+}
+"
 
-p_e = Phi(beta_e);
-p_s = Phi(beta_s);
-p_e_diff = p_e[2]-p_e[1];
-p_s_diff = p_s[2]-p_s[1];
+
+
+mod_bb_jnt_code2 <- "
+// model 2 
+
+functions {
+#include /bb_normCop_funs.stan
+}
+data {
+#include /bb_short_dat.stan
+}
+parameters {
+// params for binary efficacy (beta_e) and safety (beta_s) outcomes
+vector[2] beta_e; 
+vector[2] beta_s;
+
+// copula dependence param
+vector<lower=-1, upper=1>[2] rho_;  
+}
+model {
+vector[N] pr_e;
+vector[N] pr_s;
+vector[N] theta;
+
+// priors
+// weakly informative priors for beta_e and beta_s
+beta_e ~ normal(0,10);
+beta_s ~ normal(0,10);
+
+// weakly informative prior for rho
+// add hyperprior for variance of rho prior??
+rho_ ~ normal(0,0.4); 
+
+// marginal probit models for binary efficacy and safety outcomes
+pr_e = Phi(x*beta_e);
+pr_s = Phi(x*beta_s);
+
+// copula dependence model
+theta = x*rho_;
+
+// build log-likelihood
+{
+  vector[N] loglik;  // vectorize summation
+  for (i in 1:N)
+  loglik[i] = freq[i]*bi_cop_lp(y_e[i], pr_e[i], y_s[i], pr_s[i], theta[i]);
+  target += sum(loglik);
+}
+
+}
+generated quantities {
+#include /bb_normCop_genquants.stan
+}
+"
+
+mod_bb_jnt_code3 <- "
+// model 3
+
+functions {
+#include /bb_normCop_funs.stan
+}
+data {
+#include /bb_short_dat.stan
+}
+parameters {
+// params for binary efficacy (beta_e) and safety (beta_s) outcomes
+vector[2] beta_e; 
+vector[2] beta_s;
+
+// unscaled copula dependence param 
+vector<lower=0, upper=1>[2] rho0;  
+}
+transformed parameters {
+// scaled copula dependence param 
+vector<lower=-1, upper=1>[2] rho_ = 2*(rho0 - 0.5);
+}
+model {
+vector[N] pr_e;
+vector[N] pr_s;
+vector[N] theta;
+
+// priors
+// weakly informative priors for beta_e and beta_s
+beta_e ~ normal(0,10);
+beta_s ~ normal(0,10);
+
+// boundary-avoiding beta() prior on unscaled rho0
+// see https://github.com/stan-dev/stan/wiki/Prior-Choice-Recommendations
+// --> Boundary-avoiding priors for modal estimation
+rho0 ~ beta(2,2); 
+
+// marginal probit models for binary efficacy and safety outcomes
+pr_e = Phi(x*beta_e);
+pr_s = Phi(x*beta_s);
+
+// copula dependence model
+theta = x*rho_;
+
+// build log-likelihood
+{
+  vector[N] loglik;  // vectorize summation
+  for (i in 1:N)
+  loglik[i] = freq[i]*bi_cop_lp(y_e[i], pr_e[i], y_s[i], pr_s[i], theta[i]);
+  target += sum(loglik);
+}
+
+}
+generated quantities {
+#include /bb_normCop_genquants.stan
 }
 "
 
 
 ## Compile and save models
 
+if (0){
 # efficacy marginal model
 mod_bb_e <- stan_model(model_code = mod_bb_e_code)
 saveRDS(mod_bb_e, file = file.path(getwd(),"mod_bb_e_local.rds"))
@@ -139,7 +231,14 @@ saveRDS(mod_bb_e, file = file.path(getwd(),"mod_bb_e_local.rds"))
 # safety marginal model
 mod_bb_s <- stan_model(model_code = mod_bb_s_code)
 saveRDS(mod_bb_s, file = file.path(getwd(),"mod_bb_s_local.rds"))
+}
 
 # joint models
-mod_bb_jnt <- stan_model(model_code = mod_bb_jnt_code)
-saveRDS(mod_bb_jnt, file = file.path(getwd(),"mod_bb_jnt_local.rds"))
+mod_bb_jnt1 <- stan_model(model_code = mod_bb_jnt_code1)
+saveRDS(mod_bb_jnt1, file = file.path(getwd(),"mod_bb_jnt1_local.rds"))
+
+mod_bb_jnt2 <- stan_model(model_code = mod_bb_jnt_code2)
+saveRDS(mod_bb_jnt2, file = file.path(getwd(),"mod_bb_jnt2_local.rds"))
+
+mod_bb_jnt3 <- stan_model(model_code = mod_bb_jnt_code3)
+saveRDS(mod_bb_jnt3, file = file.path(getwd(),"mod_bb_jnt3_local.rds"))
